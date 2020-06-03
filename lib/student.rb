f98e9fc48add992e868c14ce7ca7fcfb0a1569f4
Vlad <VladDaImpaler@gmail.com>
2020-06-03 14:05:05 -0400

    finished student.rb

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment.rb"
+require "pry"
 
 class Student
   attr_accessor :name, :grade
@@ -7,9 +8,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def initialize(id=nil, name, grade)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
@@ -34,12 +36,11 @@ class Student
 
   def save
     if self.id
-      #self.update
+      self.update
     else
       sql_query = <<-SQL
         INSERT INTO students (name, grade) VALUES (?, ?)
       SQL
-
       DB[:conn].execute(sql_query, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
@@ -52,10 +53,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
+    id, name, grade = row
+    student = self.new(name, grade, id)
     student
   end
 
@@ -63,9 +62,16 @@ class Student
     sql_query = <<-SQL
       SELECT * FROM students WHERE name = ?;
       SQL
-      row = DB[:conn].execute(sql_query, name)
+      row = DB[:conn].execute(sql_query, name)[0]
       new_from_db(row)
   end
 
-  #def update
+  def update
+    sql_command = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?;
+      SQL
+      DB[:conn].execute(sql_command, self.name, self.grade, self.id)
+  end
 end
+
+#To clarify, I am not working on Tic Tac Toe, my account was paused because payment hasn't been received yet, even though it has been withdrawn from my bank account already.... So I am continuing while this stuff gets resolved by tomorrow
